dc57eed211ccff2c312b9965661fb7ab9a1b13ad
Davi Nickenig Cassitas Barbosa <devdavinickenig@gmail.com>
2023-10-13 00:24:36 -0300

    Projeto Dia 1 Finalizado
    Projeto finalizado, bem próximo ao original proposto no exercício.

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 function tocarSom(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return
+
+    key.classList.add('tecla__pressionada')
     audio.currentTime = 0;
     audio.play();
 }
 
+function removerPressionada(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('tecla__pressionada')
+}
 
-// const keys = Array.from(document.querySelector('.som'));
 
+const keys = Array.from(document.querySelectorAll(".tecla"));
+keys.forEach(key => key.addEventListener("transitionend", removerPressionada))
 window.addEventListener('keydown', tocarSom)
 
 
-// function playSound(e) {
-//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-//     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-//     if (!audio) return;
-
-//     key.classList.add('playing');
-//     audio.currentTime = 0;
-//     audio.play();
-//   }
-
-//   const keys = Array.from(document.querySelectorAll('.som'));
-//   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-//   window.addEventListener('keydown', playSound);
